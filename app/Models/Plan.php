a08455038a205e121b77a82d385a97a98837f79d
Flávio Paixão <flavio_pc@outlook.com>
2020-10-21 10:51:00 -0300

    Relacionamento de planos com perfis

--- app/Models/Plan.php
+++ app/Models/Plan.php
@@ -6,19 +6,41 @@ use Illuminate\Database\Eloquent\Model;
 
 class Plan extends Model
 {
-    protected $fillable = ['name','url','price','description'];
+    protected $fillable = ['name', 'url', 'price', 'description'];
 
     public function details()
     {
         return $this->hasMany(DetailPlan::class);
     }
 
+    public function profiles()
+    {
+        return $this->belongsToMany(Profile::class);
+    }
+
     public function search($filter = null)
     {
-        $result = $this->where('name','LIKE',"%{$filter}%")
-                        ->orWhere('description','LIKE',"%{$filter}%")
-                        ->paginate();
+        $result = $this->where('name', 'LIKE', "%{$filter}%")
+            ->orWhere('description', 'LIKE', "%{$filter}%")
+            ->paginate();
 
         return $result;
+    }
+
+    public function profilesAvailable($filter = null)
+    {
+
+        $profiles = Profile::whereNotIn('profiles.id', function ($query) {
+            $query->select('plan_profile.profile_id');
+            $query->from('plan_profile');
+            $query->whereRaw("plan_profile.plan_id={$this->id}");
+        })
+            ->where(function ($queryFilter) use ($filter) {
+                if ($filter)
+                    $queryFilter->where('profiles.name', 'LIKE', "%{$filter}%");
+            })
+            ->paginate();
+
+        return $profiles;
     }
 }
