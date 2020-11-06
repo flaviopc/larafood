ef480f9e0ee209d71f65e3e76bf73d121e41d0d3
Flávio Paixão <flavio_pc@outlook.com>
2020-11-06 08:39:49 -0300

    instalação do telescope

--- routes/api.php
+++ routes/api.php
@@ -1,30 +1,18 @@
 <?php
 
-use Illuminate\Http\Request;
+Route::group([
+    'prefix' => 'v1',
+    'namespace' => 'Api'
+], function () {
+    Route::get('/tenant/{uuid}', 'TenantApiController@show');
+    Route::get('/tenants', 'TenantApiController@index');
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+    Route::get('/categories/{url}', 'CategoryApiController@show');
+    Route::get('/categories', 'CategoryApiController@categoriesByTenant');
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+    Route::get('/tables/{identify}', 'TableApiController@show');
+    Route::get('/tables', 'TableApiController@tablesByTenant');
+
+    Route::get('/products/{flag}', 'ProductApiController@show');
+    Route::get('/products', 'ProductApiController@productsByTenant');
 });
-
-Route::get('/tenant/{uuid}', 'Api\TenantApiController@show');
-Route::get('/tenants', 'Api\TenantApiController@index');
-
-Route::get('/categories/{url}', 'Api\CategoryApiController@show');
-Route::get('/categories', 'Api\CategoryApiController@categoriesByTenant');
-
-Route::get('/tables/{identify}', 'Api\TableApiController@show');
-Route::get('/tables', 'Api\TableApiController@tablesByTenant');
-
-Route::get('/products/{flag}', 'Api\ProductApiController@show');
-Route::get('/products', 'Api\ProductApiController@productsByTenant');
